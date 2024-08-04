396cf10b734b24ef4e5d1ab87428e20d45eac010
anusiyagwebtech <141471962+anusiyagwebtech@users.noreply.github.com>
2024-08-04 10:26:31 +0530

    Create JavaProblems.java

---
+++ JavaProblems.java
@@ -0,0 +1,12 @@
+class HelloWorld {
+    public static void main(String[] args) {
+    //   Find even or odd
+    int num = 7;
+    if(num % 2 == 0) {
+        System.out.println("Number is even");
+    }else {
+        System.out.println("Number is odd");
+    }
+    
+    }
+}
